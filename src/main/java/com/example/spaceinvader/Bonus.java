31d42720f3128d1d93f49c1d148c643401d3c324
KarolWes <karol.wesolowski01@gmail.com>
2021-11-27 16:35:03 +0100

    dodane grafiki na bonusy, poprawka w bonusie

--- src/main/java/com/example/spaceinvader/Bonus.java
+++ src/main/java/com/example/spaceinvader/Bonus.java
@@ -2,10 +2,13 @@ package com.example.spaceinvader;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,36 +16,35 @@ public class Bonus extends Movable implements Hitting{
     private int speed;
     private int value;
     @FXML
-    private Circle skin;
+    private ImageView skin;
     private int x;
     private int y;
     private boolean generated = false;
-    private List<Color>fillings;
+    private List<String>fillings;
 
     public Bonus(int x, int y, int speed) {
         this.x = x;
         this.y = y;
         this.speed = speed;
-        value = (int)(Math.random()*5);
+
         fillings = new ArrayList<>();
-        fillings.add(Color.BLUE); // shield
-        fillings.add(Color.SILVER); // double shot
-        fillings.add(Color.BROWN); // slowdown
-        fillings.add(Color.RED); // add hearts
-        fillings.add(Color.YELLOW); // coin
-        skin = new Circle();
-        skin.setRadius(12.0);
-        skin.setCenterX(x+skin.getRadius());
-        skin.setCenterY(y+skin.getRadius());
-        skin.setFill(fillings.get(value));
+        fillings.add("src/main/resources/com/example/spaceinvader/bonus_shield.png"); // shield
+        fillings.add("src/main/resources/com/example/spaceinvader/bonus_double.png"); // shield
+        fillings.add("src/main/resources/com/example/spaceinvader/bonus_time.png"); // shield
+        fillings.add("src/main/resources/com/example/spaceinvader/bonus_heart.png"); // shield
+        fillings.add("src/main/resources/com/example/spaceinvader/bonus_coin.png"); // shield
+        value = (int)(Math.random()*5);
+        skin = new ImageView((new File(fillings.get(value)).toURI().toString()));
+        skin.setX(this.x+0.5*skin.getFitHeight());
+        skin.setY(this.y+0.5*skin.getFitWidth());
     }
 
     public void move(){
         List<Integer> pos = move(0, speed, x, y);
         x = pos.get(0);
         y = pos.get(1);
-        skin.setCenterX(x+skin.getRadius());
-        skin.setCenterY(y+skin.getRadius());
+        skin.setX(this.x+0.5*skin.getFitHeight());
+        skin.setY(this.y+0.5*skin.getFitWidth());
     }
 
     @Override
@@ -63,7 +65,7 @@ public class Bonus extends Movable implements Hitting{
         return generated;
     }
 
-    public Circle getSkin() {
+    public ImageView getSkin() {
         return skin;
     }
 
